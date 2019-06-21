18fe8c0e9ca86fd590e4f0bd82a86a900da4aa74
t1s1 <tedesi@gmail.com>
2019-06-21 13:17:10 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
-def hello():
-	return "Hello, I'm Pathfinder 2"
+def main():
+    return render_template('index.html')
+if __name__ == "__main__":
+    app.run()
