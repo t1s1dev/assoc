42e55e7cfa05a2c7a81ccfae46a345223b202be3
ted singdahlsen <ted.singdahlsen@globalknowledge.com>
2019-06-25 13:52:49 -0400

    test save data

--- application.py
+++ application.py
@@ -7,8 +7,13 @@ def main():
     return render_template('index.html')
 
 @app.route('/getData/<path:filename>')
-def getData(filename):
+def get_data(filename):
     return send_from_directory('./static/data/', filename)
+
+@app.route('/saveData')
+def save_data():
+    with open("test.txt","wb") as fo:
+    	fo.write("This is Test Data")
 
 if __name__ == "__main__":
     app.run()
