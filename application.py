4329d0045fccc25a5560b2542177e36897bbb3f6
t1s1 <tedesi@gmail.com>
2019-06-25 09:14:18 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
+
 @app.route("/")
-def root():
-    return app.send_static_file('index.html')
+
+def main():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
