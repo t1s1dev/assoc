9b287812f1d372d473057cfc9b226859d3a23d0a
ted singdahlsen <ted.singdahlsen@globalknowledge.com>
2019-06-25 10:36:08 -0400

    serving CSV

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def main():
     return render_template('index.html')
+
+@app.route('/getCoursesData')
+def getCoursesData():
+    return send_from_directory('data/', 'AA_courses.csv', as_attachment=True)
 
 if __name__ == "__main__":
     app.run()
